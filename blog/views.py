8cf265e4ec99641f67491fafcdec6c5d1f571e83
phares <abungaphares@gmail.com>
2016-03-03 10:20:49 +0300

    index and home views added

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,8 @@ def index(request):
 
     data = {'mydata': "Heloo hello"}
     return render(request, 'blog/index.html', data)
+
+def home(request):
+
+    data = {'mydata': "home home home"}
+    return render(request, 'blog/home.html', data)
